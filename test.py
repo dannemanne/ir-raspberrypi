d04a117435838b0afbebdb207aeb0b6b179b0fed
Daniel Wiklund <daniel@happyrabbit.com>
2012-09-08 18:17:26 +0800

    Adding test file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ GPIO.setup(3, GPIO.IN)
 
 i = 0
 while i < 10:
-    start_time = time.clock()
+    start_time = str(time.clock())
     irinput = GPIO.input(3)
-    print ''.join([start_time, time.clock()])
+    print ''.join([start_time, str(time.clock())])
     i += 1
